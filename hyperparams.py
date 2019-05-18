d8db626b7f663c7086180a41488fc25d88ca9beb
Yash Jakhotiya <mailsforyashj@gmail.com>
2019-05-19 01:00:09 +0530

    Create a separate class for hyperparameters

---
+++ hyperparams.py
@@ -0,0 +1,8 @@
+class Hyperparams:
+    usable_data = 2980
+    train_batch_size = 32
+    test_batch_size = 32
+    look_back_limit = 60
+    num_train = 2560
+    num_test = 360
+    num_epochs = 50
